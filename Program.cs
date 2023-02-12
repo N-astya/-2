568c613d8c5025c9b1d9cd3e7ab0c678817d3dbb
adsfg <234>
2023-02-12 19:20:47 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,114 @@
+﻿namespace Interface
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Book book = new Book();
+            Book secondBook = new Book();
+            Magazine magazine = new Magazine();
+            Magazine secondMagazine = new Magazine();
+            Printable[] printable = { book, secondMagazine, magazine, secondBook };
+            foreach (Printable item in printable)
+            {
+                item.Print();
+            }
+
+
+            Book.printBook(printable);
+            Magazine.printMagazines(printable);
+            Guitar guitar = new Guitar(12);
+            Drum drum = new Drum(50);
+            Trumpet trumpet = new Trumpet(175);
+            Instrument[] instruments = { guitar, drum, trumpet };
+            foreach (Instrument item in instruments)
+            {
+                item.Play();
+            }
+        }
+    }
+    interface Printable
+    {
+        void Print();
+    }
+    class Book : Printable
+    {
+        public static void printBook(Printable[] printable)
+        {
+            foreach (var item in printable)
+            {
+                if (item.GetType() == typeof(Book))
+                {
+                    item.Print();
+                }
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("книги");
+        }
+    }
+    class Magazine : Printable
+    {
+        public static void printMagazines(Printable[] printable)
+        {
+            foreach (var item in printable)
+            {
+                if (item.GetType() == typeof(Magazine))
+                {
+                    item.Print();
+                }
+            }
+        }
+        public void Print()
+        {
+            Console.WriteLine("журналы");
+        }
+    }
+    interface Instrument
+    {
+        void Play();
+        const string KEY = "До мажор";
+    }
+    class Guitar : Instrument
+    {
+        public Guitar(int stringsCount)
+        {
+            this.stringsCount = stringsCount;
+        }
+        private int stringsCount;
+
+        public void Play()
+        {
+            Console.WriteLine($"Играет гитара с {stringsCount} струнами");
+        }
+    }
+    class Drum : Instrument
+    {
+        public Drum(int size)
+        {
+            this.size = size;
+        }
+        private int size;
+
+        public void Play()
+        {
+            Console.WriteLine($"Играет барабан с размером: {size} см");
+        }
+    }
+    class Trumpet : Instrument
+    {
+        public Trumpet(int diameter)
+        {
+            this.diameter = diameter;
+        }
+        private int diameter;
+
+        public void Play()
+        {
+            Console.WriteLine($"Играет труба с диаметром: {diameter} см");
+        }
+    }
+}
+
